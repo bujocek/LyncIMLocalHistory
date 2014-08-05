0a4b23d3fc3a4e839e1aecdfbd5a3f8af1085aa1
bujocek <git@bujok.cz>
2014-08-05 10:20:28 +0200

    Storing conversation to a file

--- LyncIMLocalHistory/Program.cs
+++ LyncIMLocalHistory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Lync.Model;
 using Microsoft.Lync.Model.Conversation;
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace LyncIMLocalHistory
@@ -9,13 +10,19 @@ namespace LyncIMLocalHistory
     {
         public Microsoft.Lync.Model.Conversation.Conversation Conversation { get; set; }
         public DateTime ConversationCreated { get; set; }
+        public int m_convId;
     }
 
     class Program
     {
-        static Dictionary<String, ConversationContainer> ActiveConversations = new Dictionary<String, ConversationContainer>();
+        static Dictionary<Microsoft.Lync.Model.Conversation.Conversation, ConversationContainer> ActiveConversations = 
+            new Dictionary<Microsoft.Lync.Model.Conversation.Conversation, ConversationContainer>();
 
         static Self myself;
+
+        static int nextConvId = 0;
+
+        static string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
         static void Main(string[] args)
         {
@@ -29,44 +36,67 @@ namespace LyncIMLocalHistory
         static void ConversationManager_ConversationAdded(object sender, Microsoft.Lync.Model.Conversation.ConversationManagerEventArgs e)
         {
             Console.WriteLine("Conversation added.");
-            string ConversationID = e.Conversation.Properties[ConversationProperty.Id].ToString();
-            StoreConversation(e.Conversation, ConversationID);
+            ActiveConversations.Add(e.Conversation, new ConversationContainer()
+            {
+                Conversation = e.Conversation,
+                ConversationCreated = DateTime.Now,
+                m_convId = nextConvId++
+            });
+            e.Conversation.ParticipantAdded += Conversation_ParticipantAdded;
+            e.Conversation.ParticipantRemoved += Conversation_ParticipantRemoved;
+
+            //foreach (Participant participant in conversation.Participants)
+            //{
+            //    (participant.Modalities[ModalityTypes.InstantMessage] as InstantMessageModality).InstantMessageReceived += InstantMessageModality_InstantMessageReceived;
+            //    if(participant.Contact == myself.Contact)
+            //        Console.WriteLine("Found you.");
+            //    else
+            //        Console.WriteLine("Participant " + participant.Contact.GetContactInformation(ContactInformationType.DisplayName) + "found.");
+            //}
         }
 
-        private static void StoreConversation(Conversation conversation, string ConversationID)
+        static void Conversation_ParticipantRemoved(object sender, Microsoft.Lync.Model.Conversation.ParticipantCollectionChangedEventArgs args)
         {
-            ActiveConversations.Add(ConversationID, new ConversationContainer()
-            {
-                Conversation = conversation,
-                ConversationCreated = DateTime.Now
-            });
+            (args.Participant.Modalities[ModalityTypes.InstantMessage] as InstantMessageModality).InstantMessageReceived -= InstantMessageModality_InstantMessageReceived;
+            if (args.Participant.Contact == myself.Contact)
+                Console.WriteLine("You removed.");
+            else
+                Console.WriteLine("Participant removed: " + args.Participant.Contact.GetContactInformation(ContactInformationType.DisplayName));
+        }
 
-            foreach (Participant participant in conversation.Participants)
-            {
-                (participant.Modalities[ModalityTypes.InstantMessage] as InstantMessageModality).InstantMessageReceived += InstantMessageModality_InstantMessageReceived;
-                if(participant.Contact == myself.Contact)
-                    Console.WriteLine("Found you.");
-                else
-                    Console.WriteLine("Participant found.");
-            }
-            Console.WriteLine("Conversation stored.");
+        static void Conversation_ParticipantAdded(object sender, Microsoft.Lync.Model.Conversation.ParticipantCollectionChangedEventArgs args)
+        {
+            (args.Participant.Modalities[ModalityTypes.InstantMessage] as InstantMessageModality).InstantMessageReceived += InstantMessageModality_InstantMessageReceived;
+            if (args.Participant.Contact == myself.Contact)
+                Console.WriteLine("You added.");
+            else
+                Console.WriteLine("Participant added: " + args.Participant.Contact.GetContactInformation(ContactInformationType.DisplayName));
         }
 
         static void InstantMessageModality_InstantMessageReceived(object sender, Microsoft.Lync.Model.Conversation.MessageSentEventArgs args)
         {
-            
-            Console.WriteLine("Message received: " + args.Text);
+            InstantMessageModality imm = (sender as InstantMessageModality);
+            ConversationContainer container = ActiveConversations[imm.Conversation];
+            String convlog = "[" + DateTime.Now + "](" + container.m_convId + ")<" + imm.Participant.Contact.GetContactInformation(ContactInformationType.DisplayName) + "> " + args.Text;
+            using (StreamWriter outfile = new StreamWriter(mydocpath + @"\LyncIMHistory.txt"))
+            {
+                outfile.Write(convlog);
+                outfile.Close();
+            }
+            Console.WriteLine(convlog);
         }
 
         static void ConversationManager_ConversationRemoved(object sender, Microsoft.Lync.Model.Conversation.ConversationManagerEventArgs e)
         {
             string ConversationID = e.Conversation.Properties[ConversationProperty.Id].ToString();
-            if (ActiveConversations.ContainsKey(ConversationID))
+            e.Conversation.ParticipantAdded -= Conversation_ParticipantAdded;
+            e.Conversation.ParticipantRemoved -= Conversation_ParticipantRemoved;
+            if (ActiveConversations.ContainsKey(e.Conversation))
             {
-                var container = ActiveConversations[ConversationID];
+                ConversationContainer container = ActiveConversations[e.Conversation];
                 TimeSpan conversationLength = DateTime.Now.Subtract(container.ConversationCreated);
-                Console.WriteLine("Conversation {0} lasted {1} seconds", ConversationID, conversationLength);
-                ActiveConversations.Remove(ConversationID);
+                Console.WriteLine("Conversation {0} lasted {1} seconds", container.m_convId, conversationLength);
+                ActiveConversations.Remove(e.Conversation);
             }
         }
        
